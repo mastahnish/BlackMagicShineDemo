aada7db0ae637c05cde32a749c288025b7b9d9d1
jacekrys <jacek@myosolutions.pl>
2022-04-14 03:16:13 +0200

    Changed permission check from Dexter library to pure native implementation Resigned from checking the last position and building geofence around it. Only Live location will be taken into account. Corrections around PendingIntent.
    Known Issues:
    - On Android S+ Broadcast receiver is not fired so geofencing is not working
    - Noticed not operating gyroscope controls in Samsung S9

--- app/src/main/java/com/overplay/blackmagicshinedemo/presentation/MainActivity.kt
+++ app/src/main/java/com/overplay/blackmagicshinedemo/presentation/MainActivity.kt
@@ -1,33 +1,36 @@
 package com.overplay.blackmagicshinedemo.presentation
 
 import android.Manifest.permission.*
-import android.R
 import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.hardware.SensorManager
-import android.location.Location
+import android.net.Uri
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.os.Bundle
 import android.os.Looper
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.google.android.exoplayer2.util.Util
 import com.google.android.gms.location.*
 import com.google.android.material.snackbar.Snackbar
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.multi.DialogOnAnyDeniedMultiplePermissionsListener
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.overplay.blackmagicshinedemo.R
 import com.overplay.blackmagicshinedemo.constants.Constants.Companion.DELAY_IN_SECONDS
+import com.overplay.blackmagicshinedemo.constants.Constants.Companion.LOCATION_REQUEST_FASTEST_INTERVAL
+import com.overplay.blackmagicshinedemo.constants.Constants.Companion.LOCATION_REQUEST_INTERVAL
+import com.overplay.blackmagicshinedemo.constants.Constants.Companion.LOCATION_REQUEST_MAX_WAIT_TIME
 import com.overplay.blackmagicshinedemo.databinding.ActivityMainBinding
 import com.overplay.blackmagicshinedemo.extensions.countdownListener
 import com.overplay.blackmagicshinedemo.extensions.scaleAnimation
@@ -47,61 +50,39 @@ open class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
 
     private val geofencePendingIntent: PendingIntent by lazy {
         val intent = Intent(this, GeofenceBroadcastReceiver::class.java)
-        PendingIntent.getBroadcast(
-            this,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        )
+        if (Util.SDK_INT >= VERSION_CODES.S) {
+            PendingIntent.getBroadcast(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
+            )
+        } else {
+            PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
     }
 
     private val viewModel: MainViewModel by viewModels()
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var geoFencingClient: GeofencingClient
+    private var fusedLocationClient: FusedLocationProviderClient? = null
+    private var geoFencingClient: GeofencingClient? = null
+    private lateinit var locationCallback: LocationCallback
+    private val locationRequest: LocationRequest = LocationRequest.create().apply {
+        interval = LOCATION_REQUEST_INTERVAL
+        fastestInterval = LOCATION_REQUEST_FASTEST_INTERVAL
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        maxWaitTime = LOCATION_REQUEST_MAX_WAIT_TIME
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        checkPermissions()
+        checkLocationPermissions()
+
         initUi()
         initShakeSensitivity()
         initGyroscopeControls()
-    }
-
-    private fun initGyroscopeControls() {
-        gyroscope = Gyroscope(this) { xAxis, yAxis, zAxis ->
-            viewModel.controlMediaPlayerWithRotationAxis(xAxis, yAxis, zAxis)
-        }
-    }
-
-    private fun checkPermissions() {
-        Dexter.withContext(this)
-            .withPermissions(
-                ACCESS_FINE_LOCATION,
-                ACCESS_COARSE_LOCATION,
-                ACCESS_BACKGROUND_LOCATION
-            ).withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    if (report?.areAllPermissionsGranted() == true) {
-                        initGeofencing()
-                    } else {
-                        DialogOnAnyDeniedMultiplePermissionsListener.Builder
-                            .withContext(applicationContext)
-                            .withTitle(getString(com.overplay.blackmagicshinedemo.R.string.location_services_permission_dialog_title))
-                            .withMessage(getString(com.overplay.blackmagicshinedemo.R.string.location_services_permission_dialog_description))
-                            .withButtonText(R.string.ok)
-                            .withIcon(R.mipmap.sym_def_app_icon)
-                            .build()
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    p0: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                }
-            }).check()
+        initGeofencing()
     }
 
     override fun onStart() {
@@ -119,12 +100,14 @@ open class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
             initPlayerUi()
             initCountDown()
         }
+        requestLocationUpdates()
     }
 
     override fun onPause() {
         super.onPause()
         gyroscope.unregister()
         if (VERSION.SDK_INT < VERSION_CODES.N) viewModel.getMediaPlayer().release()
+        removeLocationUpdates()
     }
 
     override fun onStop() {
@@ -134,7 +117,9 @@ open class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
 
     @SuppressLint("InlinedApi")
     private fun hideSystemUi() {
-        window.setDecorFitsSystemWindows(false)
+        window?.run{
+            WindowCompat.setDecorFitsSystemWindows(this,false)
+        }
     }
 
     private fun initUi() {
@@ -173,34 +158,20 @@ open class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
         if (viewModel.getMediaPlayer().isPlaying() == true) viewModel.getMediaPlayer().pause()
     }
 
+    private fun initGyroscopeControls() {
+        gyroscope = Gyroscope(this) { xAxis, yAxis, zAxis ->
+            viewModel.controlMediaPlayerWithRotationAxis(xAxis, yAxis, zAxis)
+        }
+    }
+
     @SuppressLint("MissingPermission")
     private fun initGeofencing() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         geoFencingClient = LocationServices.getGeofencingClient(this)
 
-        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-            var lastLocationGeofencingRequest = GeofencingRequest.Builder().apply {
-                setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_EXIT)
-                addGeofence(viewModel.createGeofence(location))
-            }.build()
-
-            geoFencingClient.addGeofences(lastLocationGeofencingRequest, geofencePendingIntent)
-                .addOnSuccessListener { viewModel.getMediaPlayer().reset() }
-                .addOnFailureListener { e ->
-                    Log.e(
-                        MainActivity::javaClass.name,
-                        "${e.localizedMessage} | ${e.stackTraceToString()}"
-                    )
-                    Snackbar.make(
-                        binding.root,
-                        "${e.message}",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-        }
-
-        var locationCallback = object : LocationCallback() {
+        locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult?) {
+                Log.d(MainActivity::javaClass.name, "onLocationResult: $locationResult")
                 locationResult ?: return
 
                 var geofenceList = arrayListOf<Geofence>()
@@ -209,33 +180,172 @@ open class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
                 }
 
                 var currentLocationGeofencingRequest = GeofencingRequest.Builder().apply {
-                    setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_EXIT)
+                    setInitialTrigger(0)
                     addGeofences(geofenceList)
                 }.build()
 
-                geoFencingClient.addGeofences(
+                geoFencingClient?.addGeofences(
                     currentLocationGeofencingRequest,
                     geofencePendingIntent
                 )
-                    .addOnSuccessListener { viewModel.getMediaPlayer().reset() }
-                    .addOnFailureListener { e ->
-                        Log.e(
-                            MainActivity::javaClass.name,
-                            "${e.localizedMessage} | ${e.stackTraceToString()}"
-                        )
-                        Snackbar.make(
-                            binding.root,
-                            "${e.message}",
-                            Snackbar.LENGTH_SHORT
-                        ).show()
+                    ?.addOnSuccessListener {
+                        Log.d(MainActivity::javaClass.name, "Geofence for current location created")
+                    }
+                    ?.addOnFailureListener { e ->
+                        handleLocationException(e)
                     }
             }
         }
+    }
 
-        fusedLocationClient.requestLocationUpdates(
-            LocationRequest.create(),
-            locationCallback,
-            Looper.getMainLooper()
+    @SuppressLint("MissingPermission")
+    private fun requestLocationUpdates() {
+        if (checkLocationPermissionsGranted()) {
+            fusedLocationClient?.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    private fun removeLocationUpdates() {
+        if (checkLocationPermissionsGranted()) {
+            fusedLocationClient?.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    private fun checkLocationPermissions() {
+        if (!checkLocationPermissionsGranted()) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION) ||
+                ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_COARSE_LOCATION)
+            ) {
+                showRationaleDialog()
+            } else {
+                requestLocationPermissions()
+            }
+        } else {
+            checkBackgroundLocationPermission()
+        }
+    }
+
+    private fun checkBackgroundLocationPermission() {
+        if (checkBackgroundLocationPermissionGranted()) requestBackgroundLocationPermission()
+    }
+
+    private fun requestBackgroundLocationPermission() {
+        if (Util.SDK_INT >= VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    ACCESS_BACKGROUND_LOCATION
+                ),
+                MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION
+            )
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION),
+                MY_PERMISSIONS_REQUEST_LOCATION
+            )
+        }
+    }
+
+    private fun checkBackgroundLocationPermissionGranted(): Boolean =
+        ActivityCompat.checkSelfPermission(
+            this,
+            ACCESS_BACKGROUND_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+
+    private fun checkLocationPermissionsGranted(): Boolean = ActivityCompat.checkSelfPermission(
+        this,
+        ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+        this,
+        ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                ACCESS_FINE_LOCATION,
+                ACCESS_COARSE_LOCATION
+            ),
+            MY_PERMISSIONS_REQUEST_LOCATION
+        )
+    }
+
+    private fun showRationaleDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.location_services_permission_dialog_title))
+            .setMessage(getString(R.string.location_services_permission_dialog_description))
+            .setPositiveButton(
+                getString(R.string.ok)
+            ) { _, _ ->
+                requestLocationPermissions()
+            }
+            .create()
+            .show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    requestLocationUpdates()
+                    checkBackgroundLocationPermission()
+                } else {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            ACCESS_FINE_LOCATION
+                        ) ||
+                        !ActivityCompat.shouldShowRequestPermissionRationale(
+                            this,
+                            ACCESS_COARSE_LOCATION
+                        )
+                    ) {
+                        startActivity(
+                            Intent(
+                                ACTION_APPLICATION_DETAILS_SETTINGS,
+                                Uri.fromParts(
+                                    "com.overplay.blackmagicshinedemo",
+                                    this.packageName,
+                                    null
+                                ),
+                            ),
+                        )
+                    }
+                }
+                return
+            }
+
+            MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkLocationPermissionsGranted()) requestLocationUpdates()
+                } else {
+                    Snackbar.make(
+                        binding.root,
+                        getString(R.string.location_permissions_denied),
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+
+                return
+            }
+        }
+    }
+
+    private fun handleLocationException(e: Exception) {
+        Log.e(
+            MainActivity::javaClass.name,
+            "${e.localizedMessage} | ${e.stackTraceToString()}"
         )
     }
 
@@ -256,8 +366,22 @@ open class MainActivity : AppCompatActivity(), ShakeDetector.Listener {
             }
 
             if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                Snackbar.make(
+                    binding.root,
+                    "Geofence exited. Resetting video.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
                 viewModel.getMediaPlayer().reset()
             }
+
+            if (geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER) {
+                Snackbar.make(binding.root, "Geofence entered.", Snackbar.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_LOCATION = 1
+        private const val MY_PERMISSIONS_REQUEST_BACKGROUND_LOCATION = 2
     }
 }
